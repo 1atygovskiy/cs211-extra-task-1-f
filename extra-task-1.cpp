7a2689fde5aea24e0962a5e9f9ab099046d4dc5d
Иван Латыговский <latygovskii@sfedu.ru>
2024-10-06 20:18:46 +0300

    7

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -47,33 +47,8 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
-    /*
-        Return UTC time in time zone utc_offset.
+    return time + utc_offset;
 
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
 }
 
 int main() 
@@ -109,6 +84,15 @@ int main()
     assert(time_to_utc(-11, 18.0) == 5.0);
     assert(time_to_utc(-1, 0.0) == 1.0);
     assert(time_to_utc(-1, 23.0) == 0.0);
+    //Тесты для time_from_utc
+    assert(time_from_utc(+0, 12.0) == 12.0);
+    assert(time_from_utc(+1, 12.0) == 13.0);
+    assert(time_from_utc(-1, 12.0) == 11.0);
+    assert(time_from_utc(+6, 6.0) == 12.0);
+    assert(time_from_utc(-7, 6.0) == 23.0);
+    assert(time_from_utc(-1, 0.0) == 23.0);
+    assert(time_from_utc(-1, 23.0) == 22.0);
+    assert(time_from_utc(+1, 23.0) == 0.0);
 
     return 0;
 }
